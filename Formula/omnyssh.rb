d7d44c42b9ec5a74d281ecb80021b21de26a7aac
Your Name <your.email@example.com>
2026-05-20 10:04:36 +0400

    omnyssh v1.0.2

--- Formula/omnyssh.rb
+++ Formula/omnyssh.rb
@@ -1,22 +1,22 @@
 class Omnyssh < Formula
   desc "TUI SSH dashboard & server manager"
   homepage "https://github.com/timhartmann7/omnyssh"
-  version "1.0.1"
+  version "1.0.2"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/timhartmann7/omnyssh/releases/download/v1.0.1/omny-aarch64-apple-darwin.tar.gz"
-      sha256 "530c3bc90a14e7eba62121777143a046a2d2f34cd426c0c02b14078bc395bc54"
+      url "https://github.com/timhartmann7/omnyssh/releases/download/v1.0.2/omny-aarch64-apple-darwin.tar.gz"
+      sha256 "3baf8ed35bcd951a4e46e9de283d2b2283d8604356fff7da7e844be61a96f033"
     else
-      url "https://github.com/timhartmann7/omnyssh/releases/download/v1.0.1/omny-x86_64-apple-darwin.tar.gz"
-      sha256 "d77067f0b096ed42c9d1ad55d8ee4560a60021fe3b8d781653b153cb3d0db60a"
+      url "https://github.com/timhartmann7/omnyssh/releases/download/v1.0.2/omny-x86_64-apple-darwin.tar.gz"
+      sha256 "7767c0275a20d59115fbb16fc9ee1ed79115df9a636f7466e08b22461d009bc3"
     end
   end
 
   on_linux do
-    url "https://github.com/timhartmann7/omnyssh/releases/download/v1.0.1/omny-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "8a898d3620e8bdd7ab1cc8637b1a24f1b27135e9ae62e54145c7eea5f3e1b8f9"
+    url "https://github.com/timhartmann7/omnyssh/releases/download/v1.0.2/omny-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "dddfbb8e19826e1e63d84f29ac4cb4b1b1f50fd1bf15bd1e8cb1cc8f21f108f4"
   end
 
   def install
