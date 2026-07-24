655fb19e75166f1f8dcde2d3e1a7aab826b14c0a
Tim Hartmann <timhartmann7@proton.me>
2026-07-25 00:01:28 +0400

    bump omnyssh to 1.1.0

--- Formula/omnyssh.rb
+++ Formula/omnyssh.rb
@@ -1,22 +1,22 @@
 class Omnyssh < Formula
   desc "TUI SSH dashboard & server manager"
   homepage "https://github.com/timhartmann7/omnyssh"
-  version "1.0.4"
+  version "1.1.0"
   license "Apache-2.0"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/timhartmann7/omnyssh/releases/download/v1.0.4/omny-aarch64-apple-darwin.tar.gz"
-      sha256 "8c6074363cb3f3dea93c018fdf209941c05ff90db4b7ceaed9d73eef93bcb718"
+      url "https://github.com/timhartmann7/omnyssh/releases/download/v1.1.0/omny-aarch64-apple-darwin.tar.gz"
+      sha256 "bb298e1c8036178df831cfac6a48adb03489474c01176939215addcc7541ab6d"
     else
-      url "https://github.com/timhartmann7/omnyssh/releases/download/v1.0.4/omny-x86_64-apple-darwin.tar.gz"
-      sha256 "dd22e6dd4e374bfc52d289c4ddbe88e3d4e528c90659f06ef43c2910761cf8e5"
+      url "https://github.com/timhartmann7/omnyssh/releases/download/v1.1.0/omny-x86_64-apple-darwin.tar.gz"
+      sha256 "d38d1a74e4e162d0df339b0e797df62fba807aed5e6fb17b3de360e16157363e"
     end
   end
 
   on_linux do
-    url "https://github.com/timhartmann7/omnyssh/releases/download/v1.0.4/omny-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "be8ad90f521b4d9ac95063ae84539e651b5fc58cb6694a7645f43f5253db2a2a"
+    url "https://github.com/timhartmann7/omnyssh/releases/download/v1.1.0/omny-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "443fe8f1bdd8492487eb790738510467bf7bfb89002f3f30d5d2369379d3e80b"
   end
 
   def install
